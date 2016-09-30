648f704a991c5a2f049ccaa99fa10d8e08feb7ac
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-09-30 20:13:12 +0200

    write some of the script favn_synth_Asplices

---
+++ resynth/favn_synth_Asplices.m
@@ -0,0 +1,42 @@
+nSplices = 47;
+folder = 'A';
+
+splice_index = 1;
+
+N = 2^16;
+
+% Load target waveform
+prefix = ['F_Tele_FAVN_03_', folder, '_splice'];
+splice_str = sprintf('%0.2d', splice_index);
+file_str = [prefix, splice_str, '.wav'];
+path_str = fullfile(folder, file_str);
+[splice, sample_rate] = eca_load(path_str, N);
+
+%
+target_waveform = planck_taper(N) .* splice;
+
+%
+Q1 = 12; % number of filters per octave at first order
+T = 2^13; % amount of invariance with respect to time translation
+% The modulation setting is either 'none', 'time', or 'time-frequency'
+% The wavelets setting is either 'morlet' or 'gammatone'
+modulations = 'none';
+wavelets = 'morlet';
+N = 2^16; % length of the signal
+
+% Setup reconstruction options
+clear opts;
+opts.is_sonified = true;
+opts.is_spectrogram_displayed = false;
+% (close Figure 1 to abort early)
+opts.nIterations = 50;
+opts.sample_rate = sample_rate;
+opts.generate_text = false;
+opts.is_verbose = true;
+opts.initial_learning_rate = 0.1;
+
+%%
+archs_singlechunk = eca_setup_1chunk(Q1, T, modulations, wavelets, N);
+%%
+iterations = ...
+    eca_synthesize_1chunk(target_waveform, archs_singlechunk, opts);
